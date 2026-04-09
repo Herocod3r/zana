72827d99acc64ff6d9632167a60db47bfc3ccef5
herocod3r <jethromain@gmail.com>
2026-04-09 02:42:51 -0400

    feat(wails): hidden titlebar with full-size content on macOS

--- main.go
+++ main.go
@@ -6,29 +6,39 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
-
-	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "Zana",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
+		Title:            "Zana",
+		Width:            1200,
+		Height:           780,
+		MinWidth:         800,
+		MinHeight:        500,
+		AssetServer:      &assetserver.Options{Assets: assets},
 		BackgroundColour: &options.RGBA{R: 11, G: 11, B: 14, A: 255},
 		OnStartup:        app.startup,
-		Bind:             []interface{}{},
+		Frameless:        false,
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: true,
+				HideTitle:                  true,
+				HideTitleBar:               false,
+				FullSizeContent:            true,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.NSAppearanceNameDarkAqua,
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+		},
 	})
-
 	if err != nil {
-		println("Error:", err.Error())
+		println("error:", err.Error())
 	}
 }
